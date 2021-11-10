b88cb5e9403769cb71ba85cad360977aaaa86f7f
SUBLI-220\motegi <moteki.sublimation@gmail.com>
2021-11-10 20:10:02 +0900

    Recursive function

---
+++ Recursive function/testSource_基本再帰関数.jsx
@@ -0,0 +1,20 @@
+﻿$.writeln("*********************");
+
+var Tartget = app.project.item(8);
+$.writeln("\"Tartget name\" = "+Tartget.name);
+//$.writeln("\"testComp.parentFolde\" = "+String(testComp.parentFolder.name));
+
+
+//Get Top Folder ( Recursive function);
+function fn_GetTopFolder(Node) {
+    $.writeln("\"Node Name 1\" = "+Node.name);
+    if ( Node.parentFolder.name !== "ルート") {
+        $.writeln("\"Node ParentName\" = "+String(Node.parentFolder.name));
+        fn_GetTopFolder(Node.parentFolder);
+    }
+     $.writeln("\"Node Name 2\" = "+Node.name);
+    return Node;
+}
+
+var Node = fn_GetTopFolder(testComp);
+$.writeln("\"A Node Name 3\" = "+Node.name);
---
+++ Recursive function/testSource_片道再帰関数.jsx
@@ -0,0 +1,18 @@
+﻿$.writeln("*********************");
+
+var Target = app.project.item(8);
+
+$.writeln("\"Target Name\" = "+Target.name);
+
+//Get Top Folder ( Recursive function);
+function fn_GetTopFolder(Node) { 
+    $.writeln("\"Node Name\" = "+Node.name);
+    if ( Node.parentFolder.name !== "ルート") {
+        $.writeln("\"Node ParentName\" = "+String(Node.parentFolder.name));
+        return ( fn_GetTopFolder(Node.parentFolder) );
+    }
+    return Node;
+}
+
+var Node = fn_GetTopFolder(Target);
+$.writeln("\"Result Node Name \" = "+Node.name);
